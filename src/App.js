7f08b603659025fffd580ba75bf4a03f87b46b68
VinSjo <vincentsjogren@gmail.com>
2022-02-19 14:52:57 +0100

    added dependencies and cleanup function to useEffect in App.js

--- src/App.js
+++ src/App.js
@@ -1,119 +1,119 @@
-import "./App.css";
-import InputBox from "./components/InputBox";
-import { io } from "socket.io-client";
-import { useCallback, useEffect, useRef, useState } from "react";
-import { getRandomColor } from "./utils";
-import WaitingList from "./components/WaitingList";
-import ReactCanvasConfetti from "react-canvas-confetti";
+import './App.css';
+import InputBox from './components/InputBox';
+import { io } from 'socket.io-client';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { getRandomColor } from './utils';
+import WaitingList from './components/WaitingList';
+import ReactCanvasConfetti from 'react-canvas-confetti';
 
 function App() {
-  const [list, setList] = useState([]);
-  const socket = useRef();
-  // Timestamp: Indikation på hur längesen någon bad om hjälp
-  // ✅ Math.random för random färg
-  // Kevins MP3-ljud
-  // ✅ Namn, Breakout room (optional)
-  // Ändra favicon/title om listan är tom eller inte
-  // Wishlist: Notifikation
+	const [list, setList] = useState([]);
+	const socket = useRef();
+	// Timestamp: Indikation på hur längesen någon bad om hjälp
+	// ✅ Math.random för random färg
+	// Kevins MP3-ljud
+	// ✅ Namn, Breakout room (optional)
+	// Ändra favicon/title om listan är tom eller inte
+	// Wishlist: Notifikation
 
-  useEffect(() => {
-    socket.current = io("https://waitinglist.osuka.dev");
+	const animationInstanceRef = useRef(null);
 
-    socket.current.on("connect", () => {
-      console.log("Connected");
-    });
+	const getInstance = useCallback(instance => {
+		animationInstanceRef.current = instance;
+	}, []);
 
-    socket.current.on("new", (data) => {
-      console.log(data);
-      setList(data);
-    });
+	const makeShot = useCallback((ratio, opts) => {
+		animationInstanceRef.current &&
+			animationInstanceRef.current({
+				...opts,
+				origin: { y: 0.7 },
+				particleCount: Math.floor(200 * ratio),
+			});
+	}, []);
 
-    socket.current.on("error", (err) => {
-      console.log(err);
-    });
+	const fire = useCallback(() => {
+		makeShot(0.25, {
+			spread: 26,
+			startVelocity: 55,
+		});
 
-    socket.current.on("confetti", () => {
-      fire();
-    })
-  }, []);
+		makeShot(0.2, {
+			spread: 60,
+		});
 
-  function raiseHand(name, room) {
-    socket.current.emit("help", {
-      name: name,
-      room: room,
-      color: getRandomColor(),
-    });
-  }
+		makeShot(0.35, {
+			spread: 100,
+			decay: 0.91,
+			scalar: 0.8,
+		});
 
-  function lowerHand(id) {
-    console.log("Removed: " + id);
-    socket.current.emit("done", {
-      id: id,
-    });
-  }
+		makeShot(0.1, {
+			spread: 120,
+			startVelocity: 25,
+			decay: 0.92,
+			scalar: 1.2,
+		});
 
+		makeShot(0.1, {
+			spread: 120,
+			startVelocity: 45,
+		});
+	}, [makeShot]);
 
-  const animationInstanceRef = useRef(null);
+	useEffect(() => {
+		socket.current = io('https://waitinglist.osuka.dev');
 
-  const getInstance = useCallback((instance) => {
-    animationInstanceRef.current = instance;
-  }, []);
+		socket.current.on('connect', () => {
+			console.log('Connected');
+		});
 
-  const makeShot = useCallback((ratio, opts) => {
-    animationInstanceRef.current &&
-      animationInstanceRef.current({
-        ...opts,
-        origin: { y: 0.7 },
-        particleCount: Math.floor(200 * ratio),
-      });
-  }, []);
+		socket.current.on('new', data => {
+			console.log(data);
+			setList(data);
+		});
 
-  const fire = useCallback(() => {
-    makeShot(0.25, {
-      spread: 26,
-      startVelocity: 55,
-    });
+		socket.current.on('error', err => {
+			console.log(err);
+		});
 
-    makeShot(0.2, {
-      spread: 60,
-    });
+		socket.current.on('confetti', () => {
+			fire();
+		});
+		return () => socket.current.off();
+	}, [fire, socket]);
 
-    makeShot(0.35, {
-      spread: 100,
-      decay: 0.91,
-      scalar: 0.8,
-    });
+	function raiseHand(name, room) {
+		socket.current.emit('help', {
+			name: name,
+			room: room,
+			color: getRandomColor(),
+		});
+	}
 
-    makeShot(0.1, {
-      spread: 120,
-      startVelocity: 25,
-      decay: 0.92,
-      scalar: 1.2,
-    });
+	function lowerHand(id) {
+		console.log('Removed: ' + id);
+		socket.current.emit('done', {
+			id: id,
+		});
+	}
 
-    makeShot(0.1, {
-      spread: 120,
-      startVelocity: 45,
-    });
-  }, [makeShot]);
-
-  return (
-    <div>
-      <ReactCanvasConfetti
-        refConfetti={getInstance}
-        style={{
-          position: "fixed",
-          pointerEvents: "none",
-          width: "100%",
-          height: "100%",
-          top: 0,
-          left: 0,
-        }}
-      />
-      <InputBox onRaiseHand={raiseHand} />
-      <WaitingList onLowerHand={lowerHand} list={list} />
-    </div>
-  );
+	return (
+		<div>
+			<ReactCanvasConfetti
+				refConfetti={getInstance}
+				style={{
+					position: 'fixed',
+					pointerEvents: 'none',
+					width: '100%',
+					height: '100%',
+					top: 0,
+					left: 0,
+				}}
+			/>
+			<InputBox onRaiseHand={raiseHand} />
+			<WaitingList onLowerHand={lowerHand} list={list} />
+		</div>
+	);
 }
 
 export default App;
