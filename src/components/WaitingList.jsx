4d62733f932308ffdb33cd136127bde1d4352d78
VinSjo <vincentsjogren@gmail.com>
2022-02-19 14:51:20 +0100

    added key prop to every Item component in WaitingList

--- src/components/WaitingList.jsx
+++ src/components/WaitingList.jsx
@@ -1,17 +1,19 @@
-import React from "react";
-import Item from "./Item";
-import styles from "./Waitinglist.module.css";
+import React from 'react';
+import Item from './Item';
+import styles from './Waitinglist.module.css';
 
 function WaitingList(props) {
-
-
-  return (
-    <div className={styles.list}>
-      {props.list.map((item) => (
-        <Item item={item} onLowerHand={props.onLowerHand}/>
-      ))}
-    </div>
-  );
+	return (
+		<div className={styles.list}>
+			{props.list.map(item => (
+				<Item
+					key={item.id}
+					item={item}
+					onLowerHand={props.onLowerHand}
+				/>
+			))}
+		</div>
+	);
 }
 
 export default WaitingList;
